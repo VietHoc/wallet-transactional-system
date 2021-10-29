d63529c5b4b2b3ff727334b64a4871f790946acd
Hoc Le <viethocpro@gmail.com>
2021-10-29 23:24:38 +0700

    Build client

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  get '*path' => redirect('/')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
