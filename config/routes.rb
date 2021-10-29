4997ced1013e76c6e103931ef45e47dcacb81cf8
Hoc Le <viethocpro@gmail.com>
2021-10-29 23:33:27 +0700

    Handle refresh

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,9 @@ Rails.application.routes.draw do
         get 'search'
       end
     end
+
   end
+
+  get '*path' => redirect('/')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
