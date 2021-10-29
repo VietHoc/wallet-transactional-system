aa9f14ca66a39c586fa8312feb56eb3070339bc9
Hoc Le <viethocpro@gmail.com>
2021-10-29 23:26:45 +0700

    update router

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,5 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  get '*path' => redirect('/')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
